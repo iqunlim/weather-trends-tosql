32f417d1b896e6fa88920ac3bdf268ed86d7720b
iqunlim <mrfoobar2000@gmail.com>
2025-10-06 11:50:35 -0500

    We did it reddit

--- README.md
+++ README.md
@@ -15,3 +15,22 @@
 6. Drop unneeded columns like images and movies, including the bad row that was combined wrong
 7. Remove adult games. These tend to have skewed results and many are of poor quality and not representative of the greater dataset
 8. Finally, drop games that have no reviews. We are looking primarily at the positive and negative review counts
+
+# Kaggle
+Please check the kaggle documentation on how to pull the dataset. Please keep in mind that it is A VERY LARGE REQUEST (>1GB) in size, so try and avoid
+hitting their servers over and over.
+
+I am using the kaggle package in order to pull it, as they do not prefer direct API requests to the download endpoint without going through it and cant guarantee it will cache correctly if you do.
+
+
+# Args
+
+`--count`: Sample size to check through, defaults to 10. (I would suggest 10,000 at least)
+
+`--source`: For my dataset, please use `fronkongames/steam-games-dataset` with no url
+
+`--table`: Defaults to `api_data`
+
+`--db-path`: Database output path: defaults to `output.db`
+
+`--verbose`: FLAG: Verbose debug logging.
--- refactored_etl.py
+++ refactored_etl.py
@@ -1,6 +1,5 @@
-import os
+import logging
 import sqlite3
-from dotenv import load_dotenv
 import pandas as pd
 import kagglehub
 from pandas import DataFrame
@@ -62,36 +61,71 @@ def cronstruct_argparse():
         default=10,
         help="Count of steam games to sample from dataset. Defaults to 10.",
     )
+    parser.add_argument(
+        "-t",
+        "--table",
+        type=str,
+        default="api_data",
+        help="Database Table to place output. defaults to 'api_data'",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--source",
+        required=True,
+        type=str,
+        help="Kaggle Dataset Source. Required.",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--db-path",
+        type=str,
+        default="data.db",
+        help="Output path for sqlite database. Defaults to 'data.db'",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Display debug Information. Defaults to False",
+    )
 
     return parser
 
 
-def extract() -> DataFrame:
-    print("Fetching File from remote. This may take a moment...")
+def extract(source: str) -> DataFrame:
+    logging.debug("Fetching File from remote. This may take a moment...")
     # Download latest version
-    path = kagglehub.dataset_download("fronkongames/steam-games-dataset")
-    print("Reading csv file in to dataframe for transformations....")
+    path = kagglehub.dataset_download(source)
+    logging.debug("Reading csv file in to dataframe for transformations....")
     df = pd.read_csv(path + "/games.csv", index_col=None, header=0, names=COLS)
     return df
 
 
 def transform(df: DataFrame, sample_size=10) -> DataFrame:
-    print("Running Transforms. See Readme for details.")
+    logging.debug("Running Transforms.")
     # take a random amount of sample size n
+    logging.debug("Taking sample of size %d", sample_size)
     df = df.sample(n=sample_size)
     # sort values by app id, since they are now all over the place after the random sample
+    logging.debug("Sorting by App ID")
     df = df.sort_values("AppID")
     # then reset the index, so now the indicies are more ordered
-    df = df.reset_index(drop=True)
+    df = df.reset_index(drop=False)
 
+    logging.debug("Normalizing column names")
     # Normalize col names
     df.columns = [camel_to_sql_valid_snake(col) for col in df.columns]
 
+    logging.debug("Coming bad row with its original row")
     # combine the two that were causing csv issues:
     df["achievements"] = (
         df["achievements"].astype(str) + "," + df["bad_row"].astype(str)
     )
 
+    logging.debug("Filtering bad rows")
     # Clear out unnecessary rows that we do not need
     df = df.drop(
         [
@@ -111,38 +145,66 @@ def transform(df: DataFrame, sample_size=10) -> DataFrame:
         axis=1,
     )
 
+    logging.debug("Dropping NSFW games")
     # filter NSFW games
-    df = df[(df["tags"].str.contains("Nudity") == False)]
+    df_nonsfw = df[(df["tags"].str.contains("Nudity") == False)]
 
+    logging.debug("Filtering games with no reviews")
     # drop games with no bad reviews and no good reviews
-    df = df[((df["positive"] != 0) | (df["negative"] != 0))]
+    df_noreview = df_nonsfw[
+        ((df_nonsfw["positive"] != 0) | (df_nonsfw["negative"] != 0))
+    ]
 
     # drop games with no metacritic score
-    df = df[(df["metacritic_score"] != 0)]
+    df_cleaned = df_noreview[(df_noreview["metacritic_score"] != 0)]
 
-    return df
+    if df.empty:
+        logging.debug("DataFrame was Empty. Please run with a higher '-n' value.")
+
+    logging.debug("Complete.")
+    return df_cleaned
 
 
 def load(clean_df: DataFrame, db_file: str, table_name: str) -> None:
-    print("Loading file to database....")
+    logging.debug("Loading file to database %s on table %s", db_file, table_name)
     """Connects to the database and loads the data."""
     # Loads the clean_df to SQLite using df.to_sql().
     with sqlite3.connect(db_file) as conn:
         clean_df.to_sql(name=table_name, con=conn, if_exists="replace", index=True)
 
 
-def main(n: int):
+def main():
     """Coordinates the ETL pipeline by calling extract -> transform -> load."""
-    data = extract()
-    clean_df = transform(data, n)
-    print("Preview:\n")
-    print(clean_df.head())
-    load(clean_df, os.environ.get("DATABASE_PATH"), "games_sample")  # type: ignore
+
+    # make argparse and get args
+    parser = cronstruct_argparse()
+    args = parser.parse_args()
+
+    # set logging
+
+    logging.basicConfig(
+        level=logging.DEBUG if args.verbose else logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+
+    logging.debug("Configuration: ")
+    logging.debug("Count: %d", args.count)
+    logging.debug("Source: %s", args.source)
+    logging.debug("Database Path: %s", args.db_path)
+    logging.debug("Database Table: %s", args.table)
+
+    # ETL
+    data = extract(source=args.source)
+    clean_df = transform(data, args.count)
+    # This doesnt cleanly print to logging. so I have to use .print()
+    if args.verbose and not clean_df.empty:
+        logging.debug("Preview:\n")
+        print(clean_df.head())
+
+    load(clean_df, args.db_path, args.table)  # type: ignore
+    print("Pipeline Finished.")
 
 
 if __name__ == "__main__":
 
-    load_dotenv()
-    parser = cronstruct_argparse()
-    args = parser.parse_args()
-    main(n=args.count)
+    main()
